6b80695626215955ae74b354f9831198201aaff5
penzestamas <penzestamas8@gmail.com>
2021-05-11 12:13:55 +0200

    Router component

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
-import StorePicker from "./components/StorePicker";
-import App from "./components/App";
+import Router from "./components/Router";
 import "./css/style.css";
 
-render(<App />, document.querySelector('#root'));
+render(<Router />, document.querySelector('#root'));
