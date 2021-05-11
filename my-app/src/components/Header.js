889ef2a3631991856e64f24500c572adf26ace29
penzestamas <penzestamas8@gmail.com>
2021-05-11 11:14:22 +0200

    converting Header into function component

--- my-app/src/components/Header.js
+++ my-app/src/components/Header.js
@@ -1,27 +1,43 @@
 import React from 'react'
 
-class Header extends React.Component {
-  render() {
-    return (
-      <React.Fragment>
-        <header className="top">
-          <h1>
-            Catch
-            <span className="ofThe">
-              <span className="of">Of</span>
-              <span className="the">The</span>
-            </span>
-            Day
-          </h1>
-          <h3 className="tagline">
-            <span>{this.props.tagline}</span>
-          </h3>
-        </header>
+const Header = props => (
+  <header className="top">
+    <h1>
+      Catch
+      <span className="ofThe">
+        <span className="of">Of</span>
+        <span className="the">The</span>
+      </span>
+      Day
+    </h1>
+    <h3 className="tagline">
+      <span>{props.tagline}</span>
+    </h3>
+  </header>
+)
+
+// class Header extends React.Component {
+//   render() {
+//     return (
+//       <React.Fragment>
+//         <header className="top">
+//           <h1>
+//             Catch
+//             <span className="ofThe">
+//               <span className="of">Of</span>
+//               <span className="the">The</span>
+//             </span>
+//             Day
+//           </h1>
+//           <h3 className="tagline">
+//             <span>{this.props.tagline}</span>
+//           </h3>
+//         </header>
         
-      </React.Fragment>
+//       </React.Fragment>
       
-    )
-  }
-}
+//     )
+//   }
+// }
 
 export default Header;
