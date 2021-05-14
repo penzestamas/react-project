391aa09e8234f0536a170d1caabd7506d8c539be
penzestamas <penzestamas8@gmail.com>
2021-05-14 10:41:17 +0200

    Fish component

--- catch-of-the-day/src/components/Order.js
+++ catch-of-the-day/src/components/Order.js
@@ -3,7 +3,7 @@ import React from 'react'
 class Order extends React.Component {
   render() {
     return (
-      <div className="inventory">Order!!!!</div>
+      <div className="order">Order!!!!</div>
       
     )
   }
