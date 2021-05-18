f4568a370f6c73c1c4f7b406ba2d58c53d4bf296
penzestamas <penzestamas8@gmail.com>
2021-05-18 13:56:19 +0200

    Firebase

--- catch-of-the-day/src/base.js
+++ catch-of-the-day/src/base.js
@@ -2,9 +2,13 @@ import Rebase from 're-base';
 import firebase from 'firebase';
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyDNOinTIVdU-rZxo6zS2j67N4uk_FnRIVk",
-  authDomain: "catch-of-the-day-pt.firebaseapp.com",
-  databaseURL: "https://catch-of-the-day-pt-default-rtdb.europe-west1.firebasedatabase.app",
+  apiKey: "AIzaSyBuzSTMLOSg7DuRKgzPJRlgQCw-S2-OmO8",
+  authDomain: "cotd-pt-fixed.firebaseapp.com",
+  projectId: "cotd-pt-fixed",
+  storageBucket: "cotd-pt-fixed.appspot.com",
+  messagingSenderId: "486314137041",
+  appId: "1:486314137041:web:f8d8fb9372c170646d3cf0",
+  measurementId: "G-ZMNFB38CS3"
 })
 
 const base = Rebase.createClass(firebaseApp.database())
