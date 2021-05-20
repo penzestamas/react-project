6a2180d00eb1586628add233bc4b9062a1488de8
penzestamas <penzestamas8@gmail.com>
2021-05-20 10:14:22 +0200

    adding order to localStorage

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -14,15 +14,28 @@ class App extends React.Component {
 
   componentDidMount() {
     const params = this.props.match.params;
+    const localStorageRef = localStorage.getItem(params.storeId);
+    if(localStorageRef) {
+      console.log('Restoring it')
+      console.log(JSON.parse(localStorageRef));
+      this.setState({ order: JSON.parse(localStorageRef)})
+    }
     this.ref = base.syncState(`${params.storeId}/fishes`, {
       context: this,
       state: "fishes"
     });
   }
 
+  componentDidUpdate() {
+    console.log(this.state.order);
+    localStorage.setItem(this.props.match.params.storeId, JSON.stringify(this.state.order))
+  }
+
   componentWillUnmount() {
     base.removeBinding(this.ref);
   }
+
+  
 
   addFish = fish => {
     // 1. Take a copy of the existing state
